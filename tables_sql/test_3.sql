6e4884cdb4a2169962084344e79d8077be032417
= <=>
2019-04-26 09:38:43 +0800

    sql files

---
+++ tables_sql/test_3.sql
@@ -0,0 +1,33 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : centos
+ Source Server Type    : MySQL
+ Source Server Version : 80015
+ Source Host           : 192.168.93.90:3306
+ Source Schema         : shardingTest
+
+ Target Server Type    : MySQL
+ Target Server Version : 80015
+ File Encoding         : 65001
+
+ Date: 26/04/2019 09:37:02
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for test_3
+-- ----------------------------
+DROP TABLE IF EXISTS `test_3`;
+CREATE TABLE `test_3`  (
+  `id` bigint(20) NOT NULL,
+  `content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `ch` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `f` float(255, 0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
